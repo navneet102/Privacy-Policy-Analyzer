380c5af9edcad085139cc4800d3cbdb16883ffef
Navneet <conavneet@gmail.com>
2026-05-02 20:49:14 +0530

    Fix base url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.mode === "development" ? "http://localhost:5000" : "https://privacylens.tech",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000" : "https://privacylens.tech",
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
     },
 });
-
 export default axiosInstance;
